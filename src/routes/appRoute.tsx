8faf4ec7971066b7d0debfcf3ac51074cfce893a
ayodeji <adetayoayodeji7@gmail.com>
2025-09-19 12:47:00 +0100

    trying to find a way

--- src/routes/appRoute.tsx
+++ src/routes/appRoute.tsx
@@ -1,20 +1,34 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import SignUp from "../pages/SignUp";
 import FormPage from "../pages/FormPage";
 import SignIn from "../pages/Signin";
 
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 export default function AppRoutes() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/:eventId" element={<FormPage />} />
                 <Route path="/signin" element={<SignIn />} />
                 <Route path="/signup" element={<SignUp />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
+
+// export default function AppRoutes() {
+//     return (
+//         <BrowserRouter>
+//             <Routes>
+//                 <Route path="/:eventId" element={<FormPage />} />
+//                 <Route path="/signin" element={<SignIn />} />
+//                 <Route path="/signup" element={<SignUp />} />
+//             </Routes>
+//         </BrowserRouter>
+//     );
+// }
 
 
 
